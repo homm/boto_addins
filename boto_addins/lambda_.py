b21d45febb9d21ab21298b5c0ac6948f74a34c3e
Alexander <homm86@gmail.com>
2019-11-07 00:45:57 +0300

    Run only on python 3 and tornado 6
    Changes:
    - sqs removed
    - AsyncS3Connection.async_set_contents_from_file → set_contents_from_file
    - AsyncS3Connection.async_copy_key → copy_key
    - AsyncBucket.async_get_key_contents → get_key_contents
    - AsyncBucket.async_get_key → get_key
    - AsyncBucket.async_get_acl → get_acl
    - AsyncBucket.async_download_key → download_key
    — AsyncKey.async_exist → exist
    - async_http_download → http_download

--- boto_addins/lambda_.py
+++ boto_addins/lambda_.py
@@ -1,12 +1,10 @@
 from __future__ import unicode_literals
 
 import json
-
-from six.moves.urllib_parse import quote
+from urllib.parse import quote
 
 from botocore.auth import SigV4Auth
 from botocore.awsrequest import AWSRequest
-from tornado import gen
 from tornado.curl_httpclient import CurlAsyncHTTPClient
 from tornado.httpclient import HTTPRequest
 from yurl import URL
@@ -47,19 +45,14 @@ class Lambda(object):
 
         Usage example:
 
-          _ioloop = ioloop.IOLoop.instance()
-
-          @gen.coroutine
-          def async_request():
+          async def request():
             credentials = Credentials(access_key=<access_key>,
                                       secret_key=<secret_key>)
             payload = {'input_bucket': 'bucket', ...}
             service = Lambda('some-service', credentials, <region>)
-            result = yield service(payload)
-            _ioloop.stop()
+            result = await service(payload)
 
-          _ioloop.add_callback(async_request)
-          _ioloop.start()
+          ioloop.IOLoop.instance().run_sync(request)
 
         :param name: Name of the AWS Lambda function.
         :param credentials: AWS credentials.
@@ -91,8 +84,7 @@ class Lambda(object):
         signer.add_auth(aws_request)
         request.headers.update(dict(aws_request.headers.items()))
 
-    @gen.coroutine
-    def __call__(self, payload):
+    async def __call__(self, payload):
         """Make async call to synchronously invoke AWS Lambda function with
         `payload` data.
 
@@ -111,7 +103,7 @@ class Lambda(object):
             body=json.dumps(payload)
         )
         self.__sign_request(request)
-        response = yield self.client.fetch(request)
+        response = await self.client.fetch(request)
         error = response.headers.get('X-Amz-Function-Error', None)
         body = json.loads(response.body)
         if error:
@@ -120,4 +112,4 @@ class Lambda(object):
             raise LambdaCallError(error_type=error,
                                   message=body.get('errorMessage'),
                                   trace=body.get('stackTrace'))
-        raise gen.Return(body)
+        return body
--- boto_addins/s3.py
+++ boto_addins/s3.py
@@ -6,7 +6,8 @@ import logging
 import xml.sax
 import socket
 from uuid import uuid4
-from six.moves.urllib_parse import quote
+import asyncio
+from urllib.parse import quote
 
 from boto.handler import XmlHandler
 from boto.s3.acl import Policy
@@ -61,7 +62,7 @@ class AsyncS3Connection(S3Connection):
         )
         return fetch_request(async_request, attempts=attempts)
 
-    def async_set_contents_from_file(self, bucket, key_name, fp, headers=None,
+    async def set_contents_from_file(self, bucket, key_name, fp, headers=None,
                                      policy=None, encrypt_key=False,
                                      metadata=None, attempts=3,
                                      request_timeout=10 * 60):
@@ -73,13 +74,12 @@ class AsyncS3Connection(S3Connection):
         if metadata is not None:
             headers = merge_meta(headers, metadata, self.provider)
 
-        return self.generate_async_request(
+        return await self.generate_async_request(
             'PUT', bucket, key_name, headers=headers, body=fp.read(),
             request_timeout=request_timeout, attempts=attempts,
         )
 
-    @gen.coroutine
-    def async_copy_key(self, src_key, dst_key, temp_dir, metadata=None,
+    async def copy_key(self, src_key, dst_key, temp_dir, metadata=None,
                        policy=None, encrypt_key=False, headers=None,
                        request_timeout=10 * 60):
         headers = headers or {}
@@ -92,20 +92,19 @@ class AsyncS3Connection(S3Connection):
 
         url = src_key.generate_url(S3_TEMP_URL_TTL)
         destination = os.path.join(temp_dir, '_' + str(uuid4()))
-        yield async_http_download(url, destination,
-                                  request_timeout=request_timeout)
+        await http_download(url, destination,
+                            request_timeout=request_timeout)
 
-        @gen.coroutine
-        def producer(write):
+        async def producer(write):
             with open(destination, 'r') as f:
                 while True:
                     data = f.read(DATA_SIZE)
                     if not data:
                         break
-                    yield write(data)
+                    await write(data)
 
         try:
-            yield self.generate_async_request(
+            await self.generate_async_request(
                 'PUT', dst_key.bucket.name, dst_key.name,
                 headers=headers, body_producer=producer,
                 content_length=src_key.size,
@@ -122,13 +121,12 @@ class AsyncBucket(Bucket):
         self._downloading_files = weakref.WeakValueDictionary()
         super(AsyncBucket, self).__init__(connection, name, key_class=AsyncKey)
 
-    def async_get_key_contents(self, key_name, attempts=3, **kwargs):
-        return self.connection.generate_async_request(
+    async def get_key_contents(self, key_name, attempts=3, **kwargs):
+        return await self.connection.generate_async_request(
             'GET', self.name, key_name, attempts=attempts, **kwargs
         )
 
-    @gen.coroutine
-    def async_get_key(self, key_name, headers=None, version_id=None,
+    async def get_key(self, key_name, headers=None, version_id=None,
                       response_headers=None, attempts=3):
         query_args_l = []
         if version_id:
@@ -139,28 +137,27 @@ class AsyncBucket(Bucket):
 
         query_args = '&'.join(query_args_l) or None
         try:
-            response = yield self.connection.generate_async_request(
+            response = await self.connection.generate_async_request(
                 'HEAD', self.name, key_name,
                 headers=headers, query_args=query_args, attempts=attempts,
             )
         except httpclient.HTTPError as e:
             if e.code == 404:
-                raise gen.Return(False)
+                return False
             raise
         key = self.key_class(self)
         key.name = key_name
         clen = response.headers['content-length']
         key.size = int(clen) if clen else 0
-        raise gen.Return(key)
+        return key
 
-    @gen.coroutine
-    def async_get_acl(self, key_name='', headers=None, version_id=None,
+    async def get_acl(self, key_name='', headers=None, version_id=None,
                       attempts=3):
         query_args = 'acl'
         if version_id:
             query_args += '&versionId=%s' % version_id
 
-        resp = yield self.connection.generate_async_request(
+        resp = await self.connection.generate_async_request(
             'GET', self.name, key_name, query_args=query_args, headers=headers,
             attempts=attempts,
         )
@@ -168,34 +165,32 @@ class AsyncBucket(Bucket):
         policy = Policy(self)
         h = XmlHandler(policy, self)
         xml.sax.parseString(resp.body, h)
-        raise gen.Return(policy)
+        return policy
 
-    @gen.coroutine
-    def async_download_key(self, key, path,
+    async def download_key(self, key, path,
                            request_timeout=10 * 60, attempts=3):
         # Get shared downloading.
         future = self._downloading_files.get(key.name)
 
         if future is None:
             url = key.generate_url(S3_TEMP_URL_TTL)
-            future = async_http_download(
+            future = asyncio.ensure_future(http_download(
                 url, path, request_timeout=request_timeout, attempts=attempts,
-            )
+            ))
 
             # Share downloading.
             self._downloading_files[key.name] = future
 
         # Run task.
-        yield future
+        await future
 
 
 class AsyncKey(Key):
-    def async_exist(self):
-        return self.bucket.async_get_key(self.name)
+    async def exist(self):
+        return await self.bucket.get_key(self.name)
 
 
-@gen.coroutine
-def fetch_request(request, client=None, retry_callback=None, attempts=1):
+async def fetch_request(request, client=None, retry_callback=None, attempts=1):
     if client is None:
         client = simple_httpclient.SimpleAsyncHTTPClient()
 
@@ -211,7 +206,7 @@ def fetch_request(request, client=None, retry_callback=None, attempts=1):
     while attempts:
         wait_before_retry = 0
         try:
-            resp = yield client.fetch(request)
+            resp = await client.fetch(request)
         except httpclient.HTTPError as e:
             # retry on s3 errors
             if e.code == 500:
@@ -226,32 +221,30 @@ def fetch_request(request, client=None, retry_callback=None, attempts=1):
             except_history.append(e)
 
         else:
-            raise gen.Return(resp)
+            return resp
 
         attempts -= 1
         if not attempts:
             raise except_history[-1]
 
         if wait_before_retry:
-            yield gen.sleep(wait_before_retry)
+            await gen.sleep(wait_before_retry)
         if retry_callback:
-            yield retry_callback(request, attempts)
+            await retry_callback(request, attempts)
 
 
-@gen.coroutine
-def async_http_download(source, destination,
+async def http_download(source, destination,
                         request_timeout=10 * 60, attempts=3):
     tmp_name = '{}.{}'.format(destination, str(uuid4())[:8])
 
     try:
         with open(tmp_name, 'wb') as iobuffer:
 
-            @gen.coroutine
-            def reset_iobuffer(*args):
+            async def reset_iobuffer(*args):
                 iobuffer.seek(0)
                 iobuffer.truncate()
 
-            yield fetch_request(
+            await fetch_request(
                 httpclient.HTTPRequest(
                     source,
                     # Direct write to file.
